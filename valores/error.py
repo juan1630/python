cbe70aaa1bdfcd633383ffb486c56417b7949008
Juan <josejuanpatron1630@gmail.com>
2019-02-01 18:02:03 -0600

    Otro commit

---
+++ valores/error.py
@@ -0,0 +1,22 @@
+#Autora:Alvarado Amaro Katia Itzel
+#Materia:Metodos Numericos
+#Profesora:Maria de Lourdes Cruz Aviles
+#Grupo:2
+
+#Este programa muestra el error absoluto que se obtine con la siguiente formula que es:
+#restar el valor verdadero con el valor aproximado para que de esta manera nos de como resultado
+#el error absoluto.
+
+
+
+print("Ingresa tus valores ")
+#imprime el mensqaje, en este caso imprimira el mensaje que dice imprimir valores
+
+valorVerdadero = float(input("Ingresa el valor verdadero: "))
+#esa es la primer variable y se pone en modo float para que ejecute los numeros decimales
+valorAproximado = float(input("Ingresa el valor aproximado: ")
+)#segunda variable que al igual que la primera se declara flotante
+
+
+print(valorVerdadero-valorAproximado)
+#se hace la resta de las variables declaradas y se imprime el resultado
