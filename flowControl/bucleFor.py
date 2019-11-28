d5351561d29924ba5652434265c9dabebfc073f9
= <josejuanpatron1630@gmail.com>
2019-11-27 23:30:03 -0600

    Se vio el bucle for

---
+++ flowControl/bucleFor.py
@@ -0,0 +1,22 @@
+# No sabemos cuantas veces se va a repetir la linea de codigo
+# En python existen los bucles determinados y los indeterminados 
+# Los determinados sabemos el número de veces que se van a repetir 
+# con los ideterminados no sabemos el número de veces que se van a repetir
+
+# el bucle for cuenta con la declaracion del bucle 
+# el codigo a repetir 
+
+# Sintaxis de un bucle for
+# for varibale in elemento_a_recorrer:
+#   Cuerpo del bucle
+#   identar el cuerpo del bucle 
+# los elementos a recorrer pueden ser una lista, una tupla, un texto, un rango
+
+# declaracion del bucle 
+for i in [1,2,3,4]:
+    print("Hola", i)
+    # asigna en cada iteracion del bucle el valor a la variable i
+    #cuerpo del bucle 
+    
+for j in ["Hola", "mundo", "jejeje", "jejej"]:
+    print("Hola de nuevo", j)
--- flowControl/inOp.py
+++ flowControl/inOp.py
@@ -3,11 +3,20 @@
 
 print("Asignaturas del año 2019")
 
+# python es case sensitive 
+
+# funciones lower() y upper de igual forma están en python
+
 print("Informatica grafia - Pruebas de software - Usabilidad y accesibilidad")
 
-asignatura = input("Escribe la asignatura elegida")
+opcion = input("Escribe la asignatura elegida: ")
 
-if asignatura in ("Informatica grafica", "Pruebas de software" , "Usabilidad y accesibilidad"):
+asignatura = opcion.lower()
+print( asignatura.upper() )
+print(asignatura)
+# convertimos lo que se introdju  por teclado a minusculas 
+
+if asignatura in ("informatica grafica", "pruebas de software" , "usabilidad y accesibilidad"):
     print("Asignatura elegida")
 else:
     print("Asignatura no valida")
