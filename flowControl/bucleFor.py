41517ba6a3ed443a445754d8d0e1b5b9f3545dbc
= <josejuanpatron1630@gmail.com>
2019-11-27 23:55:33 -0600

    Se vio más del bucle for

--- flowControl/bucleFor.py
+++ flowControl/bucleFor.py
@@ -11,12 +11,39 @@
 #   Cuerpo del bucle
 #   identar el cuerpo del bucle 
 # los elementos a recorrer pueden ser una lista, una tupla, un texto, un rango
-
 # declaracion del bucle 
-for i in [1,2,3,4]:
-    print("Hola", i)
-    # asigna en cada iteracion del bucle el valor a la variable i
-    #cuerpo del bucle 
-    
-for j in ["Hola", "mundo", "jejeje", "jejej"]:
-    print("Hola de nuevo", j)
+
+# for a in ["primaver", "otoño", "verano", "invierno"]:
+#     print("Estaciones del año", a)
+
+# for i in [1,2,3,4]:
+#     print("Hola", i)
+#     # asigna en cada iteracion del bucle el valor a la variable i
+#     #cuerpo del bucle 
+
+# for j in ["Hola", "mundo", "jejeje", "jejej"]:
+#     print("Hola de nuevo", j)
+
+# el tipo range nos ayuda a usar el bucle for con los conteos numericos
+
+# for i in ["Pildoras", "informaticas", "otro"]:
+#     print("Hola", end=" ")
+
+# email = input("Ingresa un correo electronico: ")
+
+# contador  = 0
+
+# for i in email:
+#     if(i == "@" or i == "." ):
+#         contador= contador +1
+# if contador == 2:
+#     print("Correo valido")
+# else:
+#     print("Correo no es valido")
+
+
+for i in range(5):
+    print("Hola")
+# crea un array de cinco elementos 
+# lo que hace que se imprima la palabra hola 5 veces
+
